121428aa87cf2b8c5f74e3f1e4f5145fd40d00c8
jinl <jinl1874@foxmail.com>
2020-06-01 15:36:41 +0800

    修改一些配置

--- get_bing_img.py
+++ get_bing_img.py
@@ -21,18 +21,14 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')
 response = requests.get(url, headers=headers)
 count = 1
 html = etree.HTML(response.text, etree.HTMLParser())
+# 得到图片链接
 image_url = html.xpath("//head//link[@id='bgLink']/@href")[0]
 filename = html.xpath("//a[@id='sh_cp']/@title")[0]
+# 解析出图片名字
 re_object = re.match('(.*?)\s.*?\\)', filename)
 filename = re_object[1]
 pic_url = url + image_url
 filename = filename + ".jpg"
-
-
-# print(pic_url, filename)
-dir_ = r'J://Pictures//必应壁纸'
-if not os.path.exists(dir_):
-    os.makedirs(dir_)
 
 
 read = requests.get(pic_url)
@@ -41,10 +37,7 @@ img_dir = current_dir + "\\bing_img"
 if not os.path.exists(img_dir):
     os.makedirs(img_dir)
 
+# 保存图片
 with open(img_dir + '\\{}'.format(filename), 'wb') as fp:
     fp.write(read.content)
     fp.close()
-
-with open(dir_+'//{}'.format(filename), 'wb') as fp_1:
-    fp_1.write(read.content)
-    fp_1.close()
